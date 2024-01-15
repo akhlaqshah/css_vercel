e07df21623534d3da0b0e65a8a9587083c8c9027
akhlaqshah <akhlaqshah36@gmail.com>
2024-01-15 12:57:08 +0500

    added vercel config

--- css_server/css_server/settings.py
+++ css_server/css_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3w3tu3^i4fuebz^qy13^!eugr7bn*-gd(q$lr649t!dcafmsym
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -141,3 +141,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+
+STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join('staticfiles_build', 'static')
