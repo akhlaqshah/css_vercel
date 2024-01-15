fd603e50ede2ab9b3eec4cba8c3b9d9361e1e24c
akhlaqshah <akhlaqshah36@gmail.com>
2024-01-15 13:23:44 +0500

    updated commands

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,11 @@
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install django
+python3.9 -m pip install djangorestframework
+python3.9 -m pip install pyyaml
+python3.9 -m pip install requests
+python3.9 -m pip install django-cors-headers
+python3.9 -m pip install pymongo==3.12.3
+python3.9 -m pip install djongo==1.3.6
 python3.9 manage.py collectstatic --noinput --clear
 python3.9 manage.py runserver 0.0.0.0:8000
 echo "BUILD END"
